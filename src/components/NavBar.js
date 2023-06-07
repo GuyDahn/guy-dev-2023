e8fe9a6879c7216ccff8a496ea23767e523d6300
GuyDahn <guydaane@gmail.com>
2023-06-07 10:25:57 +0300

    respon menu fixed

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -209,7 +209,7 @@ const NavBar = () => {
           </nav>
 
           <nav className='flex items-center justify-center flex-wrap mt-2'>
-            <motion.a
+            {/* <motion.a
               href='https://twitter.com'
               target={'_blank'}
               whileHover={{ y: -2 }}
@@ -217,9 +217,9 @@ const NavBar = () => {
               className='w-6 mr-3 sm:mx-1'
             >
               <TwitterIcon />
-            </motion.a>
+            </motion.a> */}
             <motion.a
-              href='https://twitter.com'
+              href='https://github.com/GuyDahn'
               target={'_blank'}
               className='w-6 mx-3 bg-light rounded-full dark:bg-dark sm:mx-1'
               whileHover={{ y: -2 }}
@@ -228,7 +228,7 @@ const NavBar = () => {
               <GithubIcon />
             </motion.a>
             <motion.a
-              href='https://twitter.com'
+              href='https://linkedin.com/in/guy-dahn/'
               target={'_blank'}
               className='w-6 mx-3 sm:mx-1'
               whileHover={{ y: -2 }}
@@ -236,7 +236,7 @@ const NavBar = () => {
             >
               <LinkedInIcon />
             </motion.a>
-            <motion.a
+            {/* <motion.a
               href='https://twitter.com'
               target={'_blank'}
               className='w-6 mx-3 bg-light rounded-full sm:mx-1'
@@ -244,8 +244,8 @@ const NavBar = () => {
               whileTap={{ scale: 0.9 }}
             >
               <PinterestIcon />
-            </motion.a>
-            <motion.a
+            </motion.a> */}
+            {/* <motion.a
               href='https://twitter.com'
               target={'_blank'}
               className='w-6 mx-3 sm:mx-1'
@@ -253,7 +253,7 @@ const NavBar = () => {
               whileTap={{ scale: 0.9 }}
             >
               <DribbbleIcon />
-            </motion.a>
+            </motion.a> */}
 
             <button
               onClick={() => setMode(mode === 'light' ? 'dark' : 'light')}

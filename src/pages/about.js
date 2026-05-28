6ca009a9292b42676022f573f0c91629ddc857e6
Guy D <guydaane@gmail.com>
2026-05-28 15:48:43 +0300

    portfolio update

--- src/pages/about.js
+++ src/pages/about.js
@@ -123,7 +123,7 @@ bg-light p-8 dark:bg-dark dark:border-light xl:col-span-4 md:order-1 md:col-span
 
               <div className='flex flex-col items-end justify-center xl:items-center'>
                 <span className='inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl'>
-                  <AnimatedNumbers value={5} />+
+                  <AnimatedNumbers value={10} />+
                 </span>
                 <h2
                   className='text-xl font-medium capitalize text-dark/75 dark:text-light/75 xl:text-center md:text-lg sm:text-base
